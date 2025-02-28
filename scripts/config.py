6a5c503128a12c420dab4c85d64ed0057fd58f5d
Shaggy-Sensei <naicker.kushagra@gmail.com>
2025-02-28 10:06:43 +0530

    Update config.py

--- scripts/config.py
+++ scripts/config.py
@@ -1,6 +1,6 @@
 # Store sensitive info and constants
-ALPACA_API_KEY = "PKV0RCTYLSJPLMD90QW2"
-ALPACA_SECRET_KEY = "jUrBmkybaYVPkL3klOAHSGJNgb35mbpLXwTbJaTy"
+ALPACA_API_KEY = "ALPACA_API_KEY"
+ALPACA_SECRET_KEY = "ALPACA_SECRET_KEY"
 ALPACA_BASE_URL = "https://paper-api.alpaca.markets"  # Paper trading URL
-SYMBOL = "AAPL"  # Stock to trade
+SYMBOL = "SYMBOL"  # Stock to trade. Eg AAPL
 TIMEFRAME = "1Min"  # 1-minute bars for testing
